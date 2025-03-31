Fixed SEAT bootstrap computation

bias_bench/benchmark/seat/seat.py
@@ -32,6 +32,7 @@ class SEATRunner:
         seed=0,
         n_samples_bootstrap=1000,
         confidence_level=0.95,
+        output_file=None,
     ):
         """Initializes a SEAT test runner.
 
@@ -57,6 +58,7 @@ class SEATRunner:
         self._seed = seed
         self._n_sample_bootstrapping = n_samples_bootstrap
         self._confidence_level = confidence_level
+        self.output_file = output_file
 
     def __call__(self):
         """Runs specified SEAT tests.
@@ -79,60 +81,66 @@ class SEATRunner:
         # Use the specified tests, otherwise, run all SEAT tests.
         tests = self._tests or all_tests
 
-        results = {}
-        for test in tests:
-            print(f"Running test {test}")
 
-            # Load the test data.
-            encs = _load_json(os.path.join(self._data_dir, f"{test}{self.TEST_EXT}"))
+        if os.path.exists(self.output_file):
+            with open(self.output_file, 'r') as f:
+                results = json.load(f)
 
-            print("Computing sentence encodings")
-            encs_targ1 = _encode(
-                self._model, self._tokenizer, encs["targ1"]["examples"]
-            )
-            encs_targ2 = _encode(
-                self._model, self._tokenizer, encs["targ2"]["examples"]
-            )
-            encs_attr1 = _encode(
-                self._model, self._tokenizer, encs["attr1"]["examples"]
-            )
-            encs_attr2 = _encode(
-                self._model, self._tokenizer, encs["attr2"]["examples"]
-            )
+            results = {k: v for k, v in results.items() if k in tests}
+        else:
+            results = {}
+            for test in tests:
+                print(f"Running test {test}")
 
-            encs["targ1"]["encs"] = encs_targ1
-            encs["targ2"]["encs"] = encs_targ2
-            encs["attr1"]["encs"] = encs_attr1
-            encs["attr2"]["encs"] = encs_attr2
+                # Load the test data.
+                encs = _load_json(os.path.join(self._data_dir, f"{test}{self.TEST_EXT}"))
 
-            print("\tDone!")
+                print("Computing sentence encodings")
+                encs_targ1 = _encode(
+                    self._model, self._tokenizer, encs["targ1"]["examples"]
+                )
+                encs_targ2 = _encode(
+                    self._model, self._tokenizer, encs["targ2"]["examples"]
+                )
+                encs_attr1 = _encode(
+                    self._model, self._tokenizer, encs["attr1"]["examples"]
+                )
+                encs_attr2 = _encode(
+                    self._model, self._tokenizer, encs["attr2"]["examples"]
+                )
 
-            # Run the test on the encodings.
-            esize, pval, bs_esizes = weat.run_test(
-                encs, n_samples=self._n_samples, parametric=self._parametric, n_samples_bootstrap=self._n_sample_bootstrapping
-            )
+                encs["targ1"]["encs"] = encs_targ1
+                encs["targ2"]["encs"] = encs_targ2
+                encs["attr1"]["encs"] = encs_attr1
+                encs["attr2"]["encs"] = encs_attr2
 
-            # Step 1: Compute the mean
-            mean_score = np.mean(bs_esizes)
+                print("\tDone!")
 
-            # Step 2: Compute the standard error (SE)
-            std_error = np.std(bs_esizes, ddof=1)  # Use ddof=1 for sample std dev
-            margin_of_error = norm.ppf(1 - (1 - self._confidence_level) / 2) * std_error
+                # Run the test on the encodings.
+                esize, pval, bs_esizes = weat.run_test(
+                    encs, n_samples=self._n_samples, parametric=self._parametric, n_samples_bootstrap=self._n_sample_bootstrapping
+                )
 
-            results[test] = {
-                "experiment_id": self._experiment_id,
-                "test": test,
-                "p_value": pval,
-                "effect_size": esize,
-                "ci_scores": bs_esizes,
-                "ci_mean": mean_score,
-                "ci_margin": margin_of_error,
-            }
+                # Step 1: Compute the mean
+                mean_score = np.mean(bs_esizes)
 
+                # Step 2: Compute the standard error (SE)
+                std_error = np.std(bs_esizes, ddof=1)  # Use ddof=1 for sample std dev
+                margin_of_error = norm.ppf(1 - (1 - self._confidence_level) / 2) * std_error
+
+                results[test] = {
+                    "experiment_id": self._experiment_id,
+                    "test": test,
+                    "p_value": pval,
+                    "effect_size": esize,
+                    "ci_scores": bs_esizes,
+                    "ci_mean": mean_score,
+                    "ci_margin": margin_of_error,
+                }
 
         effect_size = np.mean(np.abs([v['effect_size'] for v in results.values()]))
-        effect_sizes = [np.mean([v['ci_scores'][i] for v in results.values()]) for i in range(self._n_sample_bootstrapping)]
-        effect_abs_sizes = np.abs(effect_sizes).tolist()
+
+        effect_abs_sizes = [np.mean([np.abs(v['ci_scores'][i]) for v in results.values()]) for i in range(self._n_sample_bootstrapping)]
         # Step 1: Compute the mean
         mean_score = np.mean(effect_abs_sizes)
 
@@ -142,7 +150,6 @@ class SEATRunner:
 
         main_results = {
             'effect_size': effect_size,
-            'ci_scores': effect_sizes,
             'ci_abs_scores': effect_abs_sizes,
             'ci_abs_mean': mean_score,
             'ci_margin': margin_of_error,
@@ -196,6 +203,7 @@ def _encode(model, tokenizer, texts):
     for text in texts:
         # Encode each example.
         inputs = tokenizer(text, return_tensors="pt")
+        inputs = {k: v.to(model.device) for k, v in inputs.items()}
         outputs = model(**inputs)
 
         # Average over the last layer of hidden representations.
@@ -203,7 +211,7 @@ def _encode(model, tokenizer, texts):
         enc = enc.mean(dim=1)
 
         # Following May et al., normalize the representation.
-        encs[text] = enc.detach().view(-1).numpy()
+        encs[text] = enc.detach().view(-1).cpu().numpy()
         encs[text] /= np.linalg.norm(encs[text])
 
     return encs